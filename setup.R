c18d0f228f55604d4bb496f9d9dad0dbc4368b1a
Sonia Mendizábal Claustro <soniame@Sosireta.local>
2024-07-18 23:23:03 +0200

    update setup R

--- setup.R
+++ setup.R
@@ -1,5 +1,19 @@
 
-# List of packages to run impact analysis 
+# R version: R version 4.4.1 (2024-06-14)
 
-packages_list <- c("tidyverse")
+# List of packages to run impact analysis 
+packages_list <- 
+  c("tidyverse", 
+    "imputeTS", 
+    "fect", 
+    "patchwork", 
+    "did", 
+    "haven",
+    "devtools")
 install.packages(packages_list)
+devtools::install_github('xuyiqing/panelView') 
+
+# List of packages to run report
+reports_list <- 
+  c("rmarkdown")
+install.packages(reports_list)
